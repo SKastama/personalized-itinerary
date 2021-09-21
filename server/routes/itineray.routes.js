af470f9ff5d90db1f53cd1d2cb97f9234a5ab1f7
Sarah Kastama <sarahkastama@gmail.com>
2021-09-21 16:57:57 -0700

    commit

--- server/routes/itineray.routes.js
+++ server/routes/itineray.routes.js
@@ -1,21 +1,16 @@
 const itinerayController = require("../controllers/itineray.controller");
+const { authenticate } = require("../config/jwt.config");
 
-/*
-LEADING SLASH REQUIRED in routes!
-Export a function to be called in server.js where the app will be passed in.
-*/
+
 module.exports = (app) => {
-    /*
-    @route("/api/itinerays")
-    def create:
-    when this URL is visited, execute the controller function.
-    */
-    app.post("/api/itinerays/admin", itinerayController.create);
-    app.post("/api/itinerays", itinerayController.create);
+    app.post("/api/register", itinerayController.register);
+    app.post("/api/login", itinerayController.login);
+    app.post("/api/logout", itinerayController.logout);
+
+    app.post("/api/itinerays", authenticate, itinerayController.create);
     app.get("/api/itinerays/all", itinerayController.getAll);
-    app.get("/api/itinerays/:id", itinerayController.getOne);
-    app.delete("/api/itinerays/:id", itinerayController.delete);
-    app.put("/api/itinerays/:id", itinerayController.update);
-    // app.post("/api/itinerays/many", itinerayController.createMany);
-    app.get("https://api.zoom.us/v2/",)
+    app.get("/api/itinerays/:id", authenticate, itinerayController.getOne);
+    app.delete("/api/itinerays/:id", authenticate, itinerayController.delete);
+    app.put("/api/itinerays/:id", authenticate, itinerayController.update);
+    // app.get("https://api.zoom.us/v2/",)
 };
