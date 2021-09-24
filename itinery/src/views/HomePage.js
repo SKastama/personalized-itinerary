82aec7e0a59eaba9f650ff89fde16c47e8d3bda7
ElvisVu <evu66@yahoo.com>
2021-09-24 11:23:28 -0700

    elvis log out changes

--- itinery/src/views/HomePage.js
+++ itinery/src/views/HomePage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link, useHistory } from "react-router-dom";
-// #comment
+// #comment food
 
 const UserList = (props) => {
     const [person, setPerson] = useState(null);
