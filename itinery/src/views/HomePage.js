dcaf051b28496f188e0abba8a6d28f5f2f6ad39e
Sarah Kastama <sarahkastama@gmail.com>
2021-09-24 11:29:52 -0700

    commit

--- itinery/src/views/HomePage.js
+++ itinery/src/views/HomePage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link, useHistory } from "react-router-dom";
-
+// Hey
 const UserList = (props) => {
     const [person, setPerson] = useState(null);
     const [needsUpdate, setNeedsUpdate] = useState(false);
